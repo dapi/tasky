d94409ecf32a4e10d93aa609eaf69688804f078f
Danil Pismenny <danil@brandymint.ru>
2019-06-06 13:59:27 +0300

    rubocop

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BoardsController < ApplicationController
   before_action :require_login
 
--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get boards_index_url
     assert_response :success
   end
-
 end
