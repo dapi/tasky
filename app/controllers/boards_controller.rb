6541c50a93136a281a0f22b91e66fbc0ccfb2bcb
Danil Pismenny <danil@brandymint.ru>
2019-06-06 20:39:07 +0300

    export board data to react

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -13,27 +13,22 @@ class BoardsController < ApplicationController
 
   private
 
+  def board
+    current_user.boards.find params[:id]
+  end
+
   def dashboard_data
     {
-      lanes: [
+      lanes: board.lanes.map do |lane|
         {
-          id: 'lane',
-          title: 'Plannet tasks',
-          label: '2/2',
-          tasks: [
-            { id: 'Task1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins',
-
-              metadata: { taskId: 'Task1' },
-              tags: [
-                { title: 'High', color: 'white', bgcolor: '#EB5A46' },
-                { title: 'Tech Debt', color: 'white', bgcolor: '#0079BF' },
-                { title: 'Very long tag that is', color: 'white', bgcolor: '#61BD4F' },
-                { title: 'One more', color: 'white', bgcolor: '#61BD4F' }
-              ] },
-            { id: 'Task2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } }
-          ]
+          id: lane.id,
+          title: lane.title,
+          # label: lane.tasks.count.to_s,
+          tasks: lane.tasks.map { |t| { id: t.id, title: t.title, description: t.detail, label: '30 mins' } }
+          # metadata: { taskId: 'Task1' },
+          # tags: [ { title: 'High', color: 'white', bgcolor: '#EB5A46' },
         }
-      ]
+      end
     }
   end
 end
