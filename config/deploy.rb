38e06cd326b28035f6f561530e00331a251ccbee
Danil Pismenny <danil@brandymint.ru>
2019-06-08 00:25:42 +0300

    tune capistrano-faster-assets to webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:BrandyMint/tasky.git' if ENV['USE_LOCAL_REPO'].ni
 set :keep_releases, 10
 
 set :linked_files, %w[config/database.yml config/master.key]
-set :linked_dirs, %w[log node_modules tmp/pids tmp/cache tmp/sockets public/assets]
+set :linked_dirs, %w[log node_modules tmp/pids tmp/cache tmp/sockets public/assets public/packs]
 
 set :config_files, fetch(:linked_files)
 
@@ -22,7 +22,7 @@ ask :branch, ENV['BRANCH'] || proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 
-set :assets_dependencies, %w[app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb config/initializers/assets.rb]
+set :assets_dependencies, %w[app/assets lib/assets vendor/assets app/javascript yarn.lock Gemfile.lock config/routes.rb config/initializers/assets.rb]
 
 set :local_assets_dir, 'public'
 
