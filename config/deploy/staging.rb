83d6dc5a30bdea92b37df578eccf52f3564c45f5
Danil Pismenny <danil@brandymint.ru>
2019-06-14 14:08:36 +0300

    Disable queue_name_prefix

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,4 +5,5 @@ set :stage, :staging
 set :rails_env, :staging
 fetch(:default_env)[:rails_env] = :staging
 
+set :sidekiq_options_per_process, ['--queue critical', '--queue critical --queue default', '--queue critical --queue mailers']
 server 'helex.brandymint.ru', user: fetch(:user), port: '22', roles: %w[sidekiq web app db bugsnag].freeze
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = "Tasky_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Tasky_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
