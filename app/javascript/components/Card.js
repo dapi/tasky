320e87997abc422c7be1de4f21ccd6b9dcf0b3c4
Danil Pismenny <danil@brandymint.ru>
2019-06-24 08:29:32 +0300

    fix bug with new created card

--- app/javascript/components/Card.js
+++ app/javascript/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ card, tagStyle }) => {
   const { id, title, description, tags, commentsCount, memberships } = card;
   const showDescription = (description || '').length > 0
   const showTags = (tags || []).length > 0
-  const showFooter = showDescription || showTags || commentsCount > 0 || memberships.length > 0
+  const showFooter = showDescription || showTags || commentsCount > 0 || (memberships || []).length > 0
   return (
     <span>
       <CardTitle>{title}</CardTitle>
@@ -17,7 +17,7 @@ const Card = ({ card, tagStyle }) => {
             {showDescription && <li className="list-inline-item"><i className="ion ion-md-list" /></li>}
             {commentsCount > 0 && <li className="list-inline-item"><i className="ion ion-md-chatboxes" /><small className="ml-1">{commentsCount}</small></li>}
             {(tags || []).map(tag => <li className="list-inline-item" key={tag.title}><Tag {...tag} tagStyle={tag.tagStyle || tagStyle} /></li>)}
-            {memberships.map(membership =>
+            {(memberships || []).map(membership =>
             <li className="list-inline-item" key={membership.id}>
               <img src={membership.avatarUrl} style={{borderRadius:'50%', marginRight: '2px'}} width={24} height={24} title={membership.publicName} alt={membership.publicName} />
             </li>
