53d8e9d434428a59a6494cf33d76199821fa69d5
Danil Pismenny <danil@brandymint.ru>
2019-06-25 12:38:13 +0300

    translate boards#new

---
+++ config/initializers/simple_form_disable_with.rb
@@ -0,0 +1,14 @@
+module DisableDoubleClickOnSimpleForms
+  def submit(field, options = {})
+    if field.is_a?(Hash)
+      field[:data] ||= {}
+      field[:data][:disable_with] ||= I18n.t('helpers.disable_with')
+    else
+      options[:data] ||= {}
+      options[:data][:disable_with] ||= I18n.t('helpers.disable_with')
+    end
+    super(field, options)
+  end
+end
+
+SimpleForm::FormBuilder.prepend(DisableDoubleClickOnSimpleForms)
