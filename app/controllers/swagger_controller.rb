98af880861aa47669929a6972c144b36e9d4a142
Danil Pismenny <danil@brandymint.ru>
2019-06-27 19:38:47 +0300

    improve swagger views

--- app/controllers/swagger_controller.rb
+++ app/controllers/swagger_controller.rb
@@ -2,12 +2,17 @@
 
 class SwaggerController < ::ApplicationController
   skip_before_action :require_login
-  layout 'swagger'
 
-  # https://github.com/yunixon/swagger-ui_rails5/blob/master/app/views/swagger_ui/_swagger_ui.html.erb
   def index
     render locals: {
-      discovery_url: request.path + '/v1/swagger_doc'
+      discovery_url: discovery_url,
+      api_key: current_user.try(:access_key)
     }
+  end
+
+  private
+
+  def discovery_url
+    url_for + '/v1/swagger_doc'
   end
 end
