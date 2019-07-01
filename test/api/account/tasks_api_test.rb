a6544c6e0eff6949f84320864baa8877b0bebfbe
Danil Pismenny <danil@brandymint.ru>
2019-07-01 14:49:12 +0300

    add tasks_api tests

---
+++ test/api/account/tasks_api_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TasksAPITest < ActionDispatch::IntegrationTest
+  setup do
+    login_user
+    account_host!
+  end
+
+  test 'GET /api/v1/tasks' do
+    create :task, account: @current_account
+    get '/api/v1/tasks'
+    assert response.successful?
+    assert_equal 1, JSON.parse(response.body)['data'].count
+  end
+
+  test 'POST /api/v1/tasks' do
+    title = generate :title
+    post '/api/v1/tasks', params: { title: title }
+    assert response.successful?
+    assert @current_account.tasks.one?
+  end
+
+  test 'DELETE /api/v1/tasks/:id' do
+    task = create :task, account: @current_account
+    delete "/api/v1/tasks/#{task.id}"
+    assert response.successful?
+    assert_raise ActiveRecord::RecordNotFound do
+      task.reload
+    end
+  end
+end
