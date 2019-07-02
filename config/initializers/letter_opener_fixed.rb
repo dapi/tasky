503d7945731867167081b4b48a13def7d4ea044a
Danil Pismenny <danil@brandymint.ru>
2019-07-02 23:30:37 +0300

    Allow LetterOpenerFixed only in development

--- config/initializers/letter_opener_fixed.rb
+++ config/initializers/letter_opener_fixed.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-ActiveSupport.on_load :action_mailer do
-  ActionMailer::Base.add_delivery_method(
-    :letter_opener_fixed,
-    LetterOpenerFixed::DeliveryMethod
-  )
+if defined? LetterOpenerFixed
+  ActiveSupport.on_load :action_mailer do
+    ActionMailer::Base.add_delivery_method(
+      :letter_opener_fixed,
+      LetterOpenerFixed::DeliveryMethod
+    )
+  end
 end
