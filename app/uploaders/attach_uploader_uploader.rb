41b5a8117219060b552bab209c9b9dc1145fdfd5
Danil Pismenny <danil@brandymint.ru>
2019-07-03 17:56:29 +0300

    Add response_content_disposition to download_url

--- app/uploaders/attach_uploader_uploader.rb
+++ app/uploaders/attach_uploader_uploader.rb
@@ -6,4 +6,10 @@ class AttachUploaderUploader < ApplicationAploader
   def store_dir
     "#{model.account.id}/tasks/#{model.id}"
   end
+
+  # You can find a full list of custom headers in AWS SDK documentation on
+  # AWS::S3::S3Object
+  def download_url(filename)
+    url(response_content_disposition: %(attachment; filename="#{filename}"))
+  end
 end
