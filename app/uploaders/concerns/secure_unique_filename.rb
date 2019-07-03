8262af6b52c5c6d9151571705ba8647f54d29753
Danil Pismenny <danil@brandymint.ru>
2019-07-03 17:34:28 +0300

    Add Uploaders

---
+++ app/uploaders/concerns/secure_unique_filename.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module SecureUniqueFilename
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.hex)
+  end
+end
