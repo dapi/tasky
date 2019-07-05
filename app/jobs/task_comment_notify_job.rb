a1ef1dbec381156f35854b7f08cd3df6273a8566
Danil Pismenny <danil@brandymint.ru>
2019-07-05 11:52:35 +0300

    minor

--- app/jobs/task_comment_notify_job.rb
+++ app/jobs/task_comment_notify_job.rb
@@ -3,7 +3,11 @@
 class TaskCommentNotifyJob < ApplicationJob
   def perform(task_comment_id)
     task_comment = TaskComment.find task_comment_id
+
+    # Notify task about changes in comments_count
     TaskChannel.broadcast_to task_comment.task, TaskSerializer.new(task_comment.task, include: [:attachments]).as_json
+
+    # Notify board about changes in comments_count
     task_comment.boards.find_each do |board|
       BoardChannel.broadcast_to(board, BoardPresenter.new(board).data)
     end
