a8cc7200cb975e1c8d89ed91cb1cf352f18c641d
Danil Pismenny <danil@brandymint.ru>
2019-07-09 22:02:20 +0300

    Zero unseen comments for fresh users

--- app/serializers/board_presenter.rb
+++ app/serializers/board_presenter.rb
@@ -29,7 +29,7 @@ class BoardPresenter
     return unless user
 
     task_user = card.task_users.find_by(user: user)
-    return card.comments_count unless task_user
+    return 0 unless task_user
 
     card.comments.where('created_at > ?', task_user.seen_at).count
   end
