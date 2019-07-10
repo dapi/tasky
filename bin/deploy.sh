19d70bf24d2396d69c6425c3b43af9feb5c15101
Danil Pismenny <danil@brandymint.ru>
2019-07-11 00:25:55 +0300

    Fix ./bin/deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,4 +4,5 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-rvm use $TRAVIS_RUBY_VERSION do bundle exec cap production deploy
+bundle exec cap production deploy
+# rvm use $TRAVIS_RUBY_VERSION do bundle exec cap production deploy
