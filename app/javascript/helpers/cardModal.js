2025049e7e456b68c42b33874f038517e52f07f1
Danil Pismenny <danil@brandymint.ru>
2019-08-31 14:17:22 +0000

    Minor refactor cardModal

--- app/javascript/helpers/cardModal.js
+++ app/javascript/helpers/cardModal.js
@@ -1,10 +1,12 @@
-const successHandler = data => {
-  $('#cardModalBody').html($(data.body).children())
-  ReactRailsUJS.mountComponents('#cardModalBody')
-}
-
 export const showCardModal = cardId => {
   const $cardModal = $('#cardModal')
+  const $body = $cardModal.find('.modal-body')
+
+  const successHandler = data => {
+    $body.html($(data.body).children())
+    ReactRailsUJS.mountComponents($body)
+  }
+
   Rails.ajax({
     url: `/cards/${cardId}`,
     type: 'get',
