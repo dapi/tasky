b82921769cf037de24b0a3edc640f227d50587de
Danil Pismenny <danil@brandymint.ru>
2019-09-02 15:06:21 +0300

    Localize layouts/mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,9 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
-    mail to: @user.mail_address
+    I18n.with_locale(choice_locale @user) do
+      mail to: @user.mail_address
+    end
   end
 
   def invite(invite_id)
@@ -12,6 +14,17 @@ class UserMailer < ApplicationMailer
     @inviter = @invite.inviter
     @url = accept_invite_url(@invite.token)
 
-    mail to: @invite.email
+    I18n.with_locale(choice_locale @inviter) do
+      mail to: @invite.email
+    end
+  end
+
+  private
+
+  def choice_locale(user)
+    # Let choice locale in mail preview
+    return locale if Rails.env.development?
+
+    user.locale || locale
   end
 end
