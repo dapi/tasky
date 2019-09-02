e2e5c7984d7ef0a6109572f0ea855eab00bd0b62
Danil Pismenny <danil@brandymint.ru>
2019-09-02 14:37:35 +0300

    Setup UserMailerPreview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,4 +2,24 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def reset_password_email
+    user.generate_reset_password_token!
+    UserMailer.reset_password_email user
+  end
+
+  def invite
+    invite = Invite.first
+    invite = Invite.create!(account: account, inviter: user, email: 'test@example.com') unless invite.present?
+    UserMailer.invite invite.id
+  end
+
+  private
+
+  def user
+    @user ||= User.first
+  end
+
+  def account
+    user.personal_account
+  end
 end
