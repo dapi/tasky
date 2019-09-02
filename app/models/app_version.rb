d8c3e3e70813f84e7c4ca9c1e3a706f6ecf2b626
Danil Pismenny <danil@brandymint.ru>
2019-09-02 15:44:22 +0300

    Use REVISION contant as semantic version metadata

--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -2,3 +2,8 @@
 
 require 'semver'
 AppVersion = SemVer.find
+
+# Deployed version has ./REVISION file in root directory
+#
+revision = Rails.root.join('REVISION')
+AppVersion.metadata = File.read(revision).chomp if File.exist? revision
