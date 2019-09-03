6f2c4d2618df81547686774a58687b616165757d
Danil Pismenny <danil@brandymint.ru>
2019-09-03 20:48:47 +0300

    Discard flash on xhr requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   include CurrentLocale
   include Flashes
 
+
   before_action :require_login
+  after_action  :clear_xhr_flash
 
   def not_found
     super
@@ -16,5 +18,12 @@ class ApplicationController < ActionController::Base
 
   def xhr_only!
     raise 'XHR only requests available' unless request.xhr?
+  end
+
+  def clear_xhr_flash
+    if request.xhr?
+      # Also modify 'flash' to other attributes which you use in your common/flashes for js
+      flash.discard
+    end
   end
 end
