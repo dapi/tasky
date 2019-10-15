c8cba2ba28f87c9610c2e3875fb92a08d8c3ac38
Danil Pismenny <danil@brandymint.ru>
2019-10-15 20:38:15 +0300

    Fix tests

--- test/controllers/board_memberships_controller_test.rb
+++ test/controllers/board_memberships_controller_test.rb
@@ -10,6 +10,7 @@ class BoardMembershipsControllerTest < ActionDispatch::IntegrationTest
   test 'should delete membership' do
     account = create :account, owner: @current_user
     board = create :board, account: account
+    assert @current_user.available_boards.include? board
     membership = create :board_membership, board: board
     delete board_membership_url(membership.id)
     assert_response :success
