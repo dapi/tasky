d5022f383bd92baf59027fcafb470bb26aacbdf3
Danil Pismenny <danil@brandymint.ru>
2021-02-06 09:16:02 +0300

    Remove redundant code

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-//environment.plugins.prepend('Provide',
-  //new webpack.ProvidePlugin({
-    //$: 'jquery/src/jquery',
-    //jQuery: 'jquery/src/jquery'
-  //})
-//)
 
 environment.plugins.
   append('Provide',
          new webpack.ProvidePlugin({
            $: 'jquery',
            jQuery: 'jquery',
-					 jquery: 'jquery',
+           jquery: 'jquery',
            Popper: ['popper.js', 'default']
          })
         )
